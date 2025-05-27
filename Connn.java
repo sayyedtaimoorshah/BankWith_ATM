ab3a1755a58a1d8dac8751cd5baf516ed4d5cc99
taimoor4377 <sayyedtaimoor1@gmail.com>
2025-05-28 03:37:29 +0500

    bug fixed

--- Connn.java
+++ Connn.java
@@ -7,7 +7,7 @@ public class Connn {
     Statement statement;
     public Connn(){
         try{
-            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/bankSystem","root","ifconfig");
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/bankSystem","root","fconfig");
             statement = connection.createStatement();
         }catch (Exception e){
             e.printStackTrace();
